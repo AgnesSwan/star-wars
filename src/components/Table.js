7ff8534e185cde93539639fac10998eb6ddae913
AgnesSwan <agnes.swan7@gmail.com>
2021-11-05 23:18:41 +0100

    added promise to get films title

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 import React, { useEffect, useMemo, useState } from "react"
 import DataTable from "react-data-table-component"
+
 const CharactersTable = () => {
     const [characters, setCharacters] = useState([])
-    const [page, setPage] = useState(1);
-    const [totalRows, setTotalRows] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [searchText, setSearchText] = useState('');
-
+    const [page, setPage] = useState(1)
+    const [totalRows, setTotalRows] = useState(0)
+    const [loading, setLoading] = useState(false)
+    const [searchText, setSearchText] = useState('')
 
     const columns = [
         {
@@ -36,33 +36,54 @@ const CharactersTable = () => {
         },
         {
             name: 'Films',
-            selector: row => row.films,
+            selector: row => row.films.map(film => <li>{film.title}</li>),
         },
-    ];
-const filteredItems = characters.filter(
-		character => character.name && character.name.toLowerCase().includes(searchText.toLowerCase()),
-	);
-const subHeaderComponentSearch= useMemo(()=>{
-    return (
-    <input type="search" placeholder="Search.." onChange={(e)=>setSearchText(e.target.value)} />
+    ]
+
+    const filteredItems = characters.filter(
+        character => character.name && character.name.toLowerCase().includes(searchText.toLowerCase()),
     )
-},[])
+
+    const subHeaderComponentSearch = useMemo(() => {
+        return (
+            <input type="search" placeholder="Search.." onChange={(e) => setSearchText(e.target.value)} />
+        )
+    }, [])
 
     useEffect(() => {
-        async function getSwapi() {
-            setLoading(true)
-            const peopleResponse = await axios.get(`https://swapi.dev/api/people/?page=${page}`)
-            for (const character of peopleResponse.data.results) {
-                const homeworld_url = character.homeworld;
-                const homeWorldResponse = await axios.get(homeworld_url);
-                character.homeworld = homeWorldResponse.data.name;
-              }
-              setLoading(false)
-              setCharacters(peopleResponse.data.results)
-              setTotalRows(peopleResponse.data.count)
-        }
-        getSwapi()
+        fetchAndDisplay()
     }, [page])
+
+    const fetchAndDisplay = async () => {
+        setLoading(true)
+        const res = await axios.get(`https://swapi.dev/api/people/?page=${page}`)
+        await displayPeople(res.data.results)
+        setTotalRows(res.data.count)
+        setLoading(false)
+    }
+
+    const displayPeople = async (data) => {
+        const personData = await Promise.all(
+            data.map(async (person) => {
+                return { ...person, films: await getFilms(person.films) }
+            })
+        )
+        for (const person of personData) {
+            const homeworld_url = person.homeworld
+            const homeWorldResponse = await axios.get(homeworld_url)
+            person.homeworld = homeWorldResponse.data.name
+        }
+        setCharacters(personData)
+    }
+
+    const getFilms = (film_urls) => {
+        return Promise.all(
+            film_urls.map(async (film_url) => {
+                const res = await axios.get(film_url)
+                return res.data
+            })
+        )
+    }
 
     return (
         <>
@@ -78,9 +99,9 @@ const subHeaderComponentSearch= useMemo(()=>{
                     noRowsPerPage: true
                 }}
                 onChangePage={page => setPage(page)}
-                progressPending={loading} 
+                progressPending={loading}
                 subHeader
-                subHeaderComponent={subHeaderComponentSearch}               
+                subHeaderComponent={subHeaderComponentSearch}
             />
         </>
     )
